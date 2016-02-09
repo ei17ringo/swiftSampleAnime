5b6527b8af6ddfc3833dfc4f973c827d15bd89e8
ei17ringo <ei17ringo@gmail.com>
2016-02-09 16:22:41 +0800

    commit init

--- sampleAnime/ViewController.swift
+++ sampleAnime/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // アニメーション対象のView
+    let aView = UIView(frame: CGRectMake(0,0,100,100))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 青色に指定
+        self.aView.backgroundColor = UIColor.blueColor()
+        
+        // 画面上に表示
+        self.view.addSubview(aView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnTap(sender: UIButton) {
+        
+        print("Tap!!!")
+        
+        //緑に変わるアニメーション
+        UIView.animateWithDuration(5, animations: { () -> Void in self.aView.backgroundColor = UIColor.greenColor()}, completion: {finished in print("色が変わりました")})
+    }
 
 }
 
